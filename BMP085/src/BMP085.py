eae5e10c079220f34a0a56694afa57165f567c82
Nicodus63 <nicolas.arveuf@gmail.com>
2016-06-26 14:30:40 +0000

    Fix code issues on BMP085

--- BMP085/src/BMP085.py
+++ BMP085/src/BMP085.py
@@ -52,26 +52,27 @@ class BMP085():
      self.pi = pi
      self.bus_handler = pi.i2c_open(self.I2C_BUS, self.BMP085_I2CADDR)
      self.mode = mode # Low power, standard, high resolution, ultra high resolution
+     self._load_calibration()
 
    def _load_calibration(self):
-     self.cal_AC1 = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC1, 2)
-     self.cal_AC2 = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC2, 2)
-     self.cal_AC3 = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC3, 2)   
-     self.cal_AC4 = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC4, 2)   
-     self.cal_AC5 = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC5, 2)   
-     self.cal_AC6 = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC6, 2)   
-     self.cal_B1  = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_B1, 2)   
-     self.cal_B2  = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_B2, 2)   
-     self.cal_MB  = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_MB, 2)   
-     self.cal_MC  = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_MC, 2)    
-     self.cal_MD  = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_MD, 2)     
+     self.cal_AC1 = self._convert_to_signed(self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC1, 2)))
+     self.cal_AC2 = self._convert_to_signed(self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC2, 2)))
+     self.cal_AC3 = self._convert_to_signed(self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC3, 2)))  
+     self.cal_AC4 = self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC4, 2)) # Not signed  
+     self.cal_AC5 = self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC5, 2)) # Not signed  
+     self.cal_AC6 = self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_AC6, 2)) # Not signed 
+     self.cal_B1  = self._convert_to_signed(self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_B1, 2)))   
+     self.cal_B2  = self._convert_to_signed(self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_B2, 2)))   
+     self.cal_MB  = self._convert_to_signed(self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_MB, 2)))   
+     self.cal_MC  = self._convert_to_signed(self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_MC, 2)))    
+     self.cal_MD  = self._convert_to_signed(self._format_data(self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_CAL_MD, 2)))     
 
    def _read_raw_temp(self):
      """Reads the raw (uncompensated) temperature from the sensor."""
      self.pi.i2c_write_byte_data(self.bus_handler, self.BMP085_CONTROL, self.BMP085_READTEMPCMD)
      time.sleep(0.005)  # Wait 5ms
      raw = self.pi.i2c_read_i2c_block_data(self.bus_handler, self.BMP085_TEMPDATA, 2)
-     return raw
+     return self._format_data(raw)
 
    def _read_raw_pressure(self):
      """Reads the raw (uncompensated) pressure level from the sensor."""
@@ -94,7 +95,7 @@ class BMP085():
      """Gets the compensated temperature in degrees celsius."""
      UT = self._read_raw_temp()
      X1 = ((UT - self.cal_AC6) * self.cal_AC5) >> 15
-     X2 = (self.cal_MC << 11) // (X1 + self.cal_MD)
+     X2 = (self.cal_MC << 11) / (X1 + self.cal_MD)
      B5 = X1 + X2
      temp = ((B5 + 8) >> 4) / 10.0
      return temp
@@ -104,7 +105,7 @@ class BMP085():
       UT = self._read_raw_temp()
       UP = self._read_raw_pressure()
       X1 = ((UT - self.cal_AC6) * self.cal_AC5) >> 15
-      X2 = (self.cal_MC << 11) // (X1 + self.cal_MD)
+      X2 = (self.cal_MC << 11) / (X1 + self.cal_MD)
       B5 = X1 + X2
 
       # Pressure Calculations
@@ -112,16 +113,16 @@ class BMP085():
       X1 = (self.cal_B2 * (B6 * B6) >> 12) >> 11
       X2 = (self.cal_AC2 * B6) >> 11
       X3 = X1 + X2
-      B3 = (((self.cal_AC1 * 4 + X3) << self._mode) + 2) // 4
+      B3 = (((self.cal_AC1 * 4 + X3) << self.mode) + 2) / 4
       X1 = (self.cal_AC3 * B6) >> 13
       X2 = (self.cal_B1 * ((B6 * B6) >> 12)) >> 16
       X3 = ((X1 + X2) + 2) >> 2
       B4 = (self.cal_AC4 * (X3 + 32768)) >> 15
-      B7 = (UP - B3) * (50000 >> self._mode)
+      B7 = (UP - B3) * (50000 >> self.mode)
       if B7 < 0x80000000:
-          p = (B7 * 2) // B4
+          p = (B7 * 2) / B4
       else:
-          p = (B7 // B4) * 2
+          p = (B7 / B4) * 2
       X1 = (p >> 8) * (p >> 8)
       X1 = (X1 * 3038) >> 16
       X2 = (-7357 * p) >> 16
@@ -141,6 +142,15 @@ class BMP085():
      pressure = float(self.read_pressure())
      p0 = pressure / pow(1.0 - altitude_m/44330.0, 5.255)
      return p0
+   
+   ''' Return one 16 bits when we rean i2c block data '''
+   def _format_data(self, tulpe):
+     return ((tulpe[1][0] << 8) + tulpe[1][1])
+     
+   def _convert_to_signed(self, x):
+     if x >= (2**16)/2:
+        x -= 2**16
+     return x
     
    def close(self):
      self.pi.i2c_close(self.bus_handler)
